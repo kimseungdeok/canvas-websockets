05f4df00cc8f74929a0519afc8534bbab3c41ec6
kimseungdeok <tmdejr1117@gmail.com>
2023-01-05 07:41:49 +0900

    Feat : deploy

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,73 +1,81 @@
-'use client'
+"use client";
 
-import { FC, useEffect, useState } from 'react'
-import { useDraw } from '../hooks/useDraw'
-import { ChromePicker } from 'react-color'
+import { FC, useEffect, useState } from "react";
+import { useDraw } from "../hooks/useDraw";
+import { ChromePicker } from "react-color";
 
-import { io } from 'socket.io-client'
-import { drawLine } from '../utils/drawLine'
-const socket = io('http://localhost:3001')
+import { io } from "socket.io-client";
+import { drawLine } from "../utils/drawLine";
+const socket = io("http://localhost:3001");
 
 interface pageProps {}
 
 type DrawLineProps = {
-  prevPoint: Point | null
-  currentPoint: Point
-  color: string
-}
+  prevPoint: Point | null;
+  currentPoint: Point;
+  color: string;
+};
 
 const page: FC<pageProps> = ({}) => {
-  const [color, setColor] = useState<string>('#000')
-  const { canvasRef, onMouseDown, clear } = useDraw(createLine)
+  const [color, setColor] = useState<string>("#000");
+  const { canvasRef, onMouseDown, clear } = useDraw(createLine);
 
   useEffect(() => {
-    const ctx = canvasRef.current?.getContext('2d')
+    const ctx = canvasRef.current?.getContext("2d");
 
-    socket.emit('client-ready')
+    socket.emit("client-ready");
 
-    socket.on('get-canvas-state', () => {
-      if (!canvasRef.current?.toDataURL()) return
-      console.log('sending canvas state')
-      socket.emit('canvas-state', canvasRef.current.toDataURL())
-    })
+    socket.on("get-canvas-state", () => {
+      if (!canvasRef.current?.toDataURL()) return;
+      console.log("sending canvas state");
+      socket.emit("canvas-state", canvasRef.current.toDataURL());
+    });
 
-    socket.on('canvas-state-from-server', (state: string) => {
-      console.log('I received the state')
-      const img = new Image()
-      img.src = state
+    socket.on("canvas-state-from-server", (state: string) => {
+      console.log("I received the state");
+      const img = new Image();
+      img.src = state;
       img.onload = () => {
-        ctx?.drawImage(img, 0, 0)
+        ctx?.drawImage(img, 0, 0);
+      };
+    });
+
+    socket.on(
+      "draw-line",
+      ({ prevPoint, currentPoint, color }: DrawLineProps) => {
+        if (!ctx) return console.log("no ctx here");
+        drawLine({ prevPoint, currentPoint, ctx, color });
       }
-    })
+    );
 
-    socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLineProps) => {
-      if (!ctx) return console.log('no ctx here')
-      drawLine({ prevPoint, currentPoint, ctx, color })
-    })
-
-    socket.on('clear', clear)
+    socket.on("clear", clear);
 
     return () => {
-      socket.off('draw-line')
-      socket.off('get-canvas-state')
-      socket.off('canvas-state-from-server')
-      socket.off('clear')
-    }
-  }, [canvasRef])
+      socket.off("draw-line");
+      socket.off("get-canvas-state");
+      socket.off("canvas-state-from-server");
+      socket.off("clear");
+    };
+  }, [canvasRef]);
 
   function createLine({ prevPoint, currentPoint, ctx }: Draw) {
-    socket.emit('draw-line', { prevPoint, currentPoint, color })
-    drawLine({ prevPoint, currentPoint, ctx, color })
+    socket.emit("draw-line", { prevPoint, currentPoint, color });
+    drawLine({ prevPoint, currentPoint, ctx, color });
   }
 
   return (
-    <div className='w-screen h-screen bg-white flex justify-center items-center'>
-      <div className='flex flex-col gap-10 pr-10'>
+    <div className="w-screen h-screen bg-white flex justify-center items-center">
+      
+      <div className="flex flex-col gap-10 pr-10">
+        <div>
+          <img src="https://miro.medium.com/max/256/1*ScCQRqiRp2NZK0yHN-moGQ.jpeg" className="w-100" />
+        </div>
         <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
         <button
-          type='button'
-          className='p-2 rounded-md border border-black'
-          onClick={() => socket.emit('clear')}>
+          type="button"
+          className="p-2 rounded-md border border-black"
+          onClick={() => socket.emit("clear")}
+        >
           Clear canvas
         </button>
       </div>
@@ -76,10 +84,10 @@ const page: FC<pageProps> = ({}) => {
         onMouseDown={onMouseDown}
         width={750}
         height={750}
-        className='border border-black rounded-md'
+        className="border border-black rounded-md"
       />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
